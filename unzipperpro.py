eee1d2ea9847316050bb83a3a2648a67d8eb3529
Siddharth Lal Deo <41829093+siddharthdeo99@users.noreply.github.com>
2023-02-12 00:18:01 +0530

    Create unzipperpro.py

---
+++ unzipperpro.py
@@ -0,0 +1,27 @@
+#Author:Siddharth deo
+import zipfile
+import tarfile
+
+def unzip(filename):
+    with zipfile.ZipFile(filename, 'r') as zip_ref:
+        zip_ref.extractall()
+
+def untar(filename):
+    with tarfile.open(filename, 'r') as tar_ref:
+        tar_ref.extractall()
+
+print('Select an option:')
+print('1. Unzip a file')
+print('2. Untar a file')
+option = input('Enter an option number: ')
+
+if option == '1':
+    filename = input('Enter the filename to unzip: ')
+    unzip(filename)
+    print('File unzipped successfully.')
+elif option == '2':
+    filename = input('Enter the filename to untar: ')
+    untar(filename)
+    print('File untarred successfully.')
+else:
+    print('Invalid option selected.')
