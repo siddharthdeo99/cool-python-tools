04837a8cfff15ab1ec9bf25e0acf46dc77b480ae
Siddharth Lal Deo <41829093+siddharthdeo99@users.noreply.github.com>
2023-02-12 00:45:48 +0530

    Smile detection
    Download haarcascade_smile.xml and haarcascade_frontalface_default.xml from xml folder

---
+++ smiledetection.py
@@ -0,0 +1,42 @@
+import cv2
+
+# Load the face and smile cascades
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
+
+# Start the video capture
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Read a frame from the video capture
+    ret, frame = cap.read()
+
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the grayscale frame
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+
+    # For each face, detect smiles
+    for (x, y, w, h) in faces:
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        smiles = smile_cascade.detectMultiScale(roi_gray, scaleFactor=1.7, minNeighbors=20)
+
+        # Draw a rectangle around the face
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+        # If a smile is detected, draw a rectangle around it
+        for (sx, sy, sw, sh) in smiles:
+            cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Smile Detector', frame)
+
+    # Exit if the 'q' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the video capture and close the window
+cap.release()
+cv2.destroyAllWindows()
